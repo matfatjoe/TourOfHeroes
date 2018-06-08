a0e99ae0643a61b9a408472558d5b90f4ebcb232
Matheus Furquim <matheus.fdecamargo@gmail.com>
2018-06-08 11:57:38 -0300

    Tutorial 10

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+﻿import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+  { id: 1, name: 'O'},
+  { id: 2, name: 'Senhor'},
+  { id: 3, name: 'É'},
+  { id: 4, name: 'Meu'},
+  { id: 5, name: 'Pastor'},
+  { id: 6, name: 'E'},
+  { id: 7, name: 'Nada'},
+  { id: 8, name: 'Me'},
+  { id: 9, name: 'Faltara'},
+  { id: 10, name: 'Salmo 23' }
+];
